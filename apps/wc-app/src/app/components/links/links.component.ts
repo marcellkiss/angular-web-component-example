b393d3300bfaff1013043fbb4a555943dfd11775
Marcell Kiss <kissmarcell88@gmail.com>
2022-01-17 08:00:17 +0100

    refactor: eslint improvement, no unused functions

--- apps/wc-app/src/app/components/links/links.component.ts
+++ apps/wc-app/src/app/components/links/links.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'wc-app-links',
   templateUrl: './links.component.html',
-  styleUrls: ['./links.component.scss']
+  styleUrls: ['./links.component.scss'],
 })
-export class LinksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class LinksComponent {}
